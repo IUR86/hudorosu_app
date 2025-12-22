854df8ba3db765e6e12e782a475045b8d4a476b3
okajima <r_okajima@lermo.co.jp>
2025-12-22 23:38:07 +0900

    環境修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         strictPort: true,
         proxy: {
             '/api': {
-                target: 'http://app:3000',
+                target: 'http://127.0.0.1:3000',
                 changeOrigin: true,
                 secure: false
             }
